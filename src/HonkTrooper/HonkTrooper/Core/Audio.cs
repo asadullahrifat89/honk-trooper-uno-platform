c2dd66a0b800fe7c715f692d5a04fd5d88121403
Asadullah Rifat <anonymusrif@gmail.com>
2023-03-15 16:05:54 +0600

    added more nugets

--- src/HonkTrooper/HonkTrooper/Core/Audio.cs
+++ src/HonkTrooper/HonkTrooper/Core/Audio.cs
@@ -1,97 +1,97 @@
 ﻿using Microsoft.UI.Xaml;
 using System;
-//using Uno.UI.Runtime.WebAssembly;
+using Uno.UI.Runtime.WebAssembly;
 
 namespace HonkTrooper
 {
-    //[HtmlElement("audio")]
-    //public partial class Audio : FrameworkElement
-    //{
-    //    #region Fields
+    [HtmlElement("audio")]
+    public partial class Audio : FrameworkElement
+    {
+        #region Fields
 
-    //    private Action _playback;
-    //    private string _baseUrl;
+        private Action _playback;
+        private string _baseUrl;
 
-    //    #endregion
+        #endregion
 
-    //    #region Methods
+        #region Methods
 
-    //    public void Initialize(
-    //        Uri uri,
-    //        double volume = 1.0,
-    //        bool loop = false,
-    //        Action playback = null)
-    //    {
-    //        var indexUrl = Uno.Foundation.WebAssemblyRuntime.InvokeJS("window.location.href;");
-    //        var appPackageId = Environment.GetEnvironmentVariable("UNO_BOOTSTRAP_APP_BASE");
+        public void Initialize(
+            Uri uri,
+            double volume = 1.0,
+            bool loop = false,
+            Action playback = null)
+        {
+            var indexUrl = Uno.Foundation.WebAssemblyRuntime.InvokeJS("window.location.href;");
+            var appPackageId = Environment.GetEnvironmentVariable("UNO_BOOTSTRAP_APP_BASE");
 
-    //        _baseUrl = $"{indexUrl}{appPackageId}";
+            _baseUrl = $"{indexUrl}{appPackageId}";
 
-    //        var audio = "element.style.display = \"none\"; " +
-    //            "element.controls = false;";
+            var audio = "element.style.display = \"none\"; " +
+                "element.controls = false;";
 
-    //        this.ExecuteJavascript(audio);
+            this.ExecuteJavascript(audio);
 
-    //        SetSource(uri);
-    //        SetVolume(volume);
-    //        SetLoop(loop);
+            SetSource(uri);
+            SetVolume(volume);
+            SetLoop(loop);
 
-    //        if (playback is not null)
-    //        {
-    //            _playback = playback;
-    //            this.RegisterHtmlEventHandler("ended", EndedEvent);
-    //        }
+            if (playback is not null)
+            {
+                _playback = playback;
+                this.RegisterHtmlEventHandler("ended", EndedEvent);
+            }
 
-    //        Console.WriteLine("source: " + uri + " volume: " + volume.ToString() + " loop: " + loop.ToString().ToLower());
-    //    }
+            Console.WriteLine("source: " + uri + " volume: " + volume.ToString() + " loop: " + loop.ToString().ToLower());
+        }
 
-    //    public void SetSource(Uri uri)
-    //    {
-    //        var source = $"{_baseUrl}/{uri.AbsoluteUri.Replace("ms-appx:///", "")}";
+        public void SetSource(Uri uri)
+        {
+            var source = $"{_baseUrl}/{uri.AbsoluteUri.Replace("ms-appx:///", "")}";
 
-    //        this.ExecuteJavascript($"element.src = \"{source}\"; ");
-    //    }
+            this.ExecuteJavascript($"element.src = \"{source}\"; ");
+        }
 
-    //    public void SetLoop(bool loop)
-    //    {
-    //        this.ExecuteJavascript($"element.loop = {loop.ToString().ToLower()};");
-    //    }
+        public void SetLoop(bool loop)
+        {
+            this.ExecuteJavascript($"element.loop = {loop.ToString().ToLower()};");
+        }
 
-    //    public void Play()
-    //    {
-    //        this.ExecuteJavascript("element.currentTime = 0; element.play();");
-    //    }
+        public void Play()
+        {
+            this.ExecuteJavascript("element.currentTime = 0; element.play();");
+        }
 
-    //    public void Stop()
-    //    {
-    //        this.ExecuteJavascript("element.pause(); element.currentTime = 0;");
-    //    }
+        public void Stop()
+        {
+            this.ExecuteJavascript("element.pause(); element.currentTime = 0;");
+        }
 
-    //    public void Pause()
-    //    {
-    //        this.ExecuteJavascript("element.pause();");
-    //    }
+        public void Pause()
+        {
+            this.ExecuteJavascript("element.pause();");
+        }
 
-    //    public void Resume()
-    //    {
-    //        this.ExecuteJavascript("element.play();");
-    //    }
+        public void Resume()
+        {
+            this.ExecuteJavascript("element.play();");
+        }
 
-    //    public void SetVolume(double volume)
-    //    {
-    //        var audio = $"element.volume = {volume}; ";
-    //        this.ExecuteJavascript(audio);
-    //    }
+        public void SetVolume(double volume)
+        {
+            var audio = $"element.volume = {volume}; ";
+            this.ExecuteJavascript(audio);
+        }
 
-    //    #endregion
+        #endregion
 
-    //    #region Events
+        #region Events
 
-    //    private void EndedEvent(object sender, EventArgs e)
-    //    {
-    //        _playback?.Invoke();
-    //    }
+        private void EndedEvent(object sender, EventArgs e)
+        {
+            _playback?.Invoke();
+        }
 
-    //    #endregion
-    //}
+        #endregion
+    }
 }
