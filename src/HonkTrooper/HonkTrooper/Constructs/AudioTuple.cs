65cca40298421050aee1a63b2990367117e6eea0
Asadullah Rifat <anonymusrif@gmail.com>
2023-03-17 19:56:50 +0600

    refact

--- src/HonkTrooper/HonkTrooper/Constructs/AudioTuple.cs
+++ src/HonkTrooper/HonkTrooper/Constructs/AudioTuple.cs
@@ -2,9 +2,9 @@
 {
     public partial class AudioTuple
     {
-        public AudioWasm[] AudioSources { get; set; }
+        public AudioWasm[] AudioSources { get; set; } // TODO: audio to implement for mobile native
 
-        public AudioWasm AudioInstance { get; set; }
+        public AudioWasm AudioInstance { get; set; } // TODO: audio to implement for mobile native
 
         public SoundType SoundType { get; set; }
 
