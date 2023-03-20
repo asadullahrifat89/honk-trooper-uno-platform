8a3b8bfe741569abb343ba106f5916febaac2008
Asadullah Rifat <anonymusrif@gmail.com>
2023-03-20 17:46:23 +0600

    refact

--- src/HonkTrooper/HonkTrooper/Extensions/ConstructExtensions.cs
+++ src/HonkTrooper/HonkTrooper/Extensions/ConstructExtensions.cs
@@ -99,7 +99,7 @@ namespace HonkTrooper
              *   ----------
              */
 
-            var diviWidth = Construct.Width / 4;            
+            var diviWidth = Construct.Width / 4;
 
             var rect = new Rect(
                 x: Construct.GetLeft() - diviWidth,
