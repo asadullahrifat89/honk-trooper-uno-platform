3283b8ed059a4bf474379d29d238667c834b86f5
Asadullah Rifat <anonymusrif@gmail.com>
2023-03-21 18:39:35 +0600

    speed offset fix for player

--- src/HonkTrooper/HonkTrooper/Constructs/Player.cs
+++ src/HonkTrooper/HonkTrooper/Constructs/Player.cs
@@ -78,7 +78,7 @@ namespace HonkTrooper
             SetChild(_content_image);
 
             IsometricDisplacement = Constants.DEFAULT_ISOMETRIC_DISPLACEMENT;
-            SpeedOffset = 0;
+            SpeedOffset = -0.5;
             DropShadowDistance = Constants.DEFAULT_DROP_SHADOW_DISTANCE;
 
             _audioStub = new AudioStub((SoundType.PLAYER_HEALTH_LOSS, 1, false));
