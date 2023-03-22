de7ee05213a2a89028483de116d7fec1c167e595
Asadullah Rifat <anonymusrif@gmail.com>
2023-03-22 16:04:22 +0600

    pixel perfecting

--- src/HonkTrooper/HonkTrooper/Constructs/Cloud.cs
+++ src/HonkTrooper/HonkTrooper/Constructs/Cloud.cs
@@ -59,7 +59,7 @@ namespace HonkTrooper
 
         public void Reset()
         {
-            SpeedOffset = _random.Next(-2, 3);
+            SpeedOffset = _random.Next(-3, 3);
 
             var uri = ConstructExtensions.GetRandomContentUri(_cloud_uris);
             _content_image.Source = new BitmapImage(uri);
