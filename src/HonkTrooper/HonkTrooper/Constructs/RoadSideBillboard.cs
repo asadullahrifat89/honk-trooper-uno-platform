11e01e9a2715455356ee25913ce75a0cca0375db
Asadullah Rifat <anonymusrif@gmail.com>
2023-03-23 17:49:07 +0600

    billboard developed

---
+++ src/HonkTrooper/HonkTrooper/Constructs/RoadSideBillboard.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Imaging;
+using System;
+using System.Linq;
+
+namespace HonkTrooper
+{
+    public partial class RoadSideBillboard : Construct
+    {
+        #region Fields
+
+        private readonly Image _content_image;
+        private readonly Uri[] _tree_uris;
+
+        #endregion
+
+        #region Ctor
+
+        public RoadSideBillboard(
+            Func<Construct, bool> animateAction,
+            Func<Construct, bool> recycleAction)
+        {
+            ConstructType = ConstructType.ROAD_SIDE_BILLBOARD;
+
+            _tree_uris = Constants.CONSTRUCT_TEMPLATES.Where(x => x.ConstructType == ConstructType.ROAD_SIDE_BILLBOARD).Select(x => x.Uri).ToArray();
+
+            var size = Constants.CONSTRUCT_SIZES.FirstOrDefault(x => x.ConstructType == ConstructType.ROAD_SIDE_BILLBOARD);
+
+            var width = size.Width;
+            var height = size.Height;
+
+            AnimateAction = animateAction;
+            RecycleAction = recycleAction;
+
+            SetSize(width: width, height: height);
+
+            var uri = ConstructExtensions.GetRandomContentUri(_tree_uris);
+
+            _content_image = new Image()
+            {
+                Source = new BitmapImage(uriSource: uri)
+            };
+
+            SetChild(_content_image);
+
+            SetSkewY(6);
+
+            SpeedOffset = Constants.DEFAULT_SPEED_OFFSET;
+            IsometricDisplacement = Constants.DEFAULT_ISOMETRIC_DISPLACEMENT;
+            DropShadowDistance = -22;
+        }
+
+        #endregion
+    }
+}
