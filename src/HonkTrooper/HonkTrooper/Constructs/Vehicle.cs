968d25aa5cd5cbb38ca09e127e74ea774c4301c4
Asadullah Rifat <anonymusrif@gmail.com>
2023-03-24 13:10:30 +0600

    vehicle base developed

--- src/HonkTrooper/HonkTrooper/Constructs/Vehicle.cs
+++ src/HonkTrooper/HonkTrooper/Constructs/Vehicle.cs
@@ -5,7 +5,7 @@ using Microsoft.UI.Xaml.Controls;
 
 namespace HonkTrooper
 {
-    public partial class Vehicle : Construct
+    public partial class Vehicle : VehicleBase
     {
         #region Fields
 
@@ -118,38 +118,6 @@ namespace HonkTrooper
 
             if (WillHonk)
                 SetHonkDelay();
-        }
-
-        public void Reposition()
-        {
-            var topOrLeft = _random.Next(2); // generate top and left corner lane wise vehicles
-            var lane = _random.Next(2); // generate number of lanes based of screen height
-
-            switch (topOrLeft)
-            {
-                case 0:
-                    {
-                        var xLaneWidth = Constants.DEFAULT_SCENE_WIDTH / 4;
-
-                        SetPosition(
-                            left: lane == 0 ? 0 - Width / 3 : (xLaneWidth - Width),
-                            top: Height * -1,
-                            z: 3);
-                    }
-                    break;
-                case 1:
-                    {
-                        var yLaneHeight = Constants.DEFAULT_SCENE_HEIGHT / 6;
-
-                        SetPosition(
-                            left: Width * -1,
-                            top: lane == 0 ? 0 - Height / 2.5 : yLaneHeight - Height / 3,
-                            z: 3);
-                    }
-                    break;
-                default:
-                    break;
-            }
         }
 
         public bool Honk()
---
+++ src/HonkTrooper/HonkTrooper/Constructs/VehicleBase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace HonkTrooper
+{
+    public partial class VehicleBase : DirectionalMovingConstruct
+    {
+        private readonly Random _random;
+
+        public VehicleBase()
+        {
+            _random = new Random();
+        }
+
+        public void Reposition()
+        {
+            var topOrLeft = _random.Next(2); // generate top and left corner lane wise vehicles
+            var lane = _random.Next(2); // generate number of lanes based of screen height
+
+            switch (topOrLeft)
+            {
+                case 0:
+                    {
+                        var xLaneWidth = Constants.DEFAULT_SCENE_WIDTH / 4;
+
+                        SetPosition(
+                            left: lane == 0 ? 0 - Width / 3 : (xLaneWidth - Width / 1.5),
+                            top: Height * -1,
+                            z: 3);
+                    }
+                    break;
+                case 1:
+                    {
+                        var yLaneHeight = Constants.DEFAULT_SCENE_HEIGHT / 6;
+
+                        SetPosition(
+                            left: Width * -1,
+                            top: lane == 0 ? 0 - Height / 2.5 : yLaneHeight - Height / 3,
+                            z: 3);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
