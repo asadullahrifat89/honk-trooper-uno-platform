85076347d940641a6db8acc76e8b4dbc64c48f82
Asadullah Rifat <anonymusrif@gmail.com>
2023-03-29 20:34:36 +0600

    assets loading screen development

--- src/HonkTrooper/HonkTrooper/Controls/HealthBar.cs
+++ src/HonkTrooper/HonkTrooper/Controls/HealthBar.cs
@@ -4,6 +4,7 @@ using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Media.Imaging;
 using System;
+using System.Runtime.CompilerServices;
 using Windows.UI;
 
 namespace HonkTrooper
