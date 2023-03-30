6d683d2624a22c8ca54b38cd8db3e8703273b53f
Asadullah Rifat <anonymusrif@gmail.com>
2023-03-30 13:01:46 +0600

    controller colors fixed

--- src/HonkTrooper/HonkTrooper/Pages/HonkBomberPage.xaml.cs
+++ src/HonkTrooper/HonkTrooper/Pages/HonkBomberPage.xaml.cs
@@ -2,7 +2,6 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Media.Animation;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
